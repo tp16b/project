1ffb0750068bbcbd3237fbeaa8f0687110a416f4
tp16b <tp_16b@163.com>
2018-08-23 00:35:34 +0800

    online-compile

--- OnlineCompile/bin/publish.sh
+++ OnlineCompile/bin/publish.sh
@@ -14,16 +14,11 @@ DST=$ROOT_PATH/../output
 
 cp $ROOT_PATH/httpd  $DST -rf; 
 
-mkdir -p  $DST/html $DST/css $DST/image $DST/book ;
+mkdir -p  $DST/html $DST/css $DST/image 
 
 cp $SRC/*.html $DST/html -rf; 
 cp $SRC/css  $DST/css  -rf; 
-cp $SRC/*.txt $DST/book -rf; 
 cp $SRC/image $DST/image -rf; 
-cp $SRC/cgi_src $DST -rf; 
 
-mkdir -p $DST/cgi_src;
-cp $SRC/cgi_src/*.c  $DST/cgi_src  -rf; 
-cp $SRC/test_cgi $DST -rf; 
 
-echo published finish! 
+echo publish done! 
